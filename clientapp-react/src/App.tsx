5feff0732b952e70be70d9f4ab60a9bb85538dd8
Fernando Aracena <aracenafernando@gmail.com>
2025-07-01 15:30:03 +0200

    resolviendo conflicto de enrutador

--- clientapp-react/src/App.tsx
+++ clientapp-react/src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import Router from './router';
-import { UserProvider } from './UserContext';
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
-interface AppProps {
-  children: React.ReactNode;
-}
-
-const App: React.FC<AppProps> = ({ children }) => {
+const App: React.FC = () => {
   return (
-    <UserProvider>
-      <div className="App bg-tangoWhite min-h-screen">
-        <Navbar />
-        <main className="pt-12 md:pt-16">
-          <Router />
-          {children}
-        </main>
-      </div>
-    </UserProvider>
+    <div className="App bg-tangoWhite min-h-screen">
+      <Navbar />
+      <main className="pt-12 md:pt-16">
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
