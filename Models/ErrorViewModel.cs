6adcd762aecefbbe31f3568f80fbc4fe0c403011
Fernando Aracena <aracenafernando@gmail.com>
2025-07-01 13:00:10 +0200

    eliminando componentes inutiles de MVC

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TangoKultura.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
