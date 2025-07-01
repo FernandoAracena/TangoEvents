9b787f49bd975f54173212c5fb695b4e83923b52
Fernando Aracena <aracenafernando@gmail.com>
2025-07-01 15:19:07 +0200

    corrigiendo de app.js a app.tsx

--- clientapp-react/src/App.tsx
+++ clientapp-react/src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import Router from './router';
 import { UserProvider } from './UserContext';
+import Navbar from './components/Navbar';
 
-function App() {
+interface AppProps {
+  children: React.ReactNode;
+}
+
+const App: React.FC<AppProps> = ({ children }) => {
   return (
     <UserProvider>
-      <Router />
+      <div className="App bg-tangoWhite min-h-screen">
+        <Navbar />
+        <main className="pt-12 md:pt-16">
+          <Router />
+          {children}
+        </main>
+      </div>
     </UserProvider>
   );
-}
+};
 
 export default App;
